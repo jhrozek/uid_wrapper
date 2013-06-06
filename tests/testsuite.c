7a9c4981ff8ac55692abc9d4db0c334e73debe61
Andreas Schneider <asn@cryptomilk.org>
2013-06-06 08:45:37 +0200

    tests: Add test_uwrap_setegid().

--- tests/testsuite.c
+++ tests/testsuite.c
@@ -35,11 +35,39 @@ static void test_uwrap_seteuid(void **state)
 	assert_int_equal(u, 42);
 }
 
+static void test_uwrap_setegid(void **state)
+{
+	int rc;
+	uid_t u;
+	char *env;
+
+	env = getenv("UID_WRAPPER");
+	if (env == NULL) {
+		printf("UID_WRAPPER env not set, uid_wrapper is disabled\n");
+		return;
+	}
+
+	(void) state; /* unused */
+
+	rc = setegid(0);
+	assert_int_equal(rc, 0);
+
+	u = getegid();
+	assert_int_equal(u, 0);
+
+	rc = setegid(42);
+	assert_int_equal(rc, 0);
+
+	u = getegid();
+	assert_int_equal(u, 42);
+}
+
 int main(void) {
 	int rc;
 
 	const UnitTest tests[] = {
 		unit_test(test_uwrap_seteuid),
+		unit_test(test_uwrap_setegid),
 	};
 
 	rc = run_tests(tests);
