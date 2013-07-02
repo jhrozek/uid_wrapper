7c3f00d92bee316651b024793c608aa92a3d71ca
Andreas Schneider <asn@cryptomilk.org>
2013-07-02 17:53:16 +0200

    cmake: Fix ctest config.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,9 +1,9 @@
 set(UPDATE_TYPE "true")
 
-set(CTEST_PROJECT_NAME "libssh")
+set(CTEST_PROJECT_NAME "uid_wrapper")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 CET")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "test.libssh.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=libssh")
+set(CTEST_DROP_SITE "mock.cryptomilk.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=uid_wrapper")
 set(CTEST_DROP_SITE_CDASH TRUE)
