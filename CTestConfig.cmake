afcd037e23437702270309cada8862b0d59a4a3a
Andreas Schneider <asn@cryptomilk.org>
2013-11-20 16:07:48 +0100

    cmake: Fix dashboard drop location.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -5,5 +5,5 @@ set(CTEST_NIGHTLY_START_TIME "01:00:00 CET")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "mock.cryptomilk.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=uid_wrapper")
+set(CTEST_DROP_LOCATION "/submit.php?project=uidwrapper")
 set(CTEST_DROP_SITE_CDASH TRUE)
