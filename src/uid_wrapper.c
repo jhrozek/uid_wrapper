2c761ef132c21ca6a79a126d74ed0915a22b1bc7
Andreas Schneider <asn@cryptomilk.org>
2014-02-01 14:32:03 +0100

    src: Use a bool for uid_wrapper_enabled().
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- src/uid_wrapper.c
+++ src/uid_wrapper.c
@@ -175,7 +175,7 @@ static pthread_mutex_t uwrap_id_mutex = PTHREAD_MUTEX_INITIALIZER;
  * UWRAP PROTOTYPES
  *********************************************************/
 
-int uid_wrapper_enabled(void);
+bool uid_wrapper_enabled(void);
 void uwrap_destructor(void) DESTRUCTOR_ATTRIBUTE;
 
 /*********************************************************
@@ -548,11 +548,11 @@ static void uwrap_init(void)
 	pthread_mutex_unlock(&uwrap_id_mutex);
 }
 
-int uid_wrapper_enabled(void)
+bool uid_wrapper_enabled(void)
 {
 	uwrap_init();
 
-	return uwrap.enabled ? 1 : 0;
+	return uwrap.enabled ? true : false;
 }
 
 static int uwrap_setresuid_thread(uid_t ruid, uid_t euid, uid_t suid)
